23fa916fa6b179847720e532d0d66df094ce1cb6
Jadson Rodrigues <jadsonbrodrigues@gmail.com>
2025-07-22 13:54:39 +0100

    todolist project added

---
+++ reactjs-fundamentos/todolist/src/App.tsx
@@ -0,0 +1,16 @@
+import Text from "./components/text"
+
+export default function App() {
+  return (
+      <div className="flex flex-col gap-2"><Text variant="body-md-bold"> 
+      Hello world!
+      </Text>
+      <Text> Levar o cão para passear!
+      </Text>
+  
+      
+      </div>
+      
+  )
+}
+
